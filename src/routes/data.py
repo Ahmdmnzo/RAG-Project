2bca629b57f08989d121faa16d56a9b2af131428
Ahmdmnzo <ahmed788799@outlook.com>
2025-12-30 23:10:54 +0200

    Add ProcessController and processing endpoint; update requirements and schemas

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI, APIRouter, Depends, UploadFile, status
 import os
 from helpers.config import get_settings, Settings
-from controllers import DataController, ProjectController
+from controllers import DataController, ProjectController, ProcessController
 from fastapi.responses import JSONResponse
 import aiofiles
 from models import ResponseSignal
 import logging 
 logger = logging.getLogger('uvicorn.error')
+
+from .schemas.data import ProcessRequest
 
 data_router = APIRouter(
     prefix="/api/v1/data",
@@ -49,3 +51,31 @@ async def upload_data(project_id: str,  file: UploadFile,
             "file_id":file_id
         }
     )
+
+@data_router.post("/process/{project_id}")
+async def process_endpoint(project_id: str, process_request: ProcessRequest):
+    file_id = process_request.file_id
+    chunk_size = process_request.chunk_size
+    overlap_size = process_request.overlap_size
+
+    process_controller = ProcessController(project_id=project_id)
+
+    file_content = process_controller.get_file_content(file_id=file_id)
+
+    file_chunks = process_controller.process_file_content(
+        file_content=file_content,
+        file_id=file_id,
+        chunk_size=chunk_size,
+        overlap_size=overlap_size
+    ) 
+
+    if file_chunks is None or len(file_chunks) == 0:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "singal": ResponseSignal.PROCESSING_FAILED.value
+            }
+        )
+
+    return file_chunks
+    
